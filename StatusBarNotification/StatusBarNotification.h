6000f76d804287a0d61dbfc2f55311bba741fd26
David Gölzhäuser <david@Davids-MacBook-Pro.local>
2017-02-11 15:47:29 +0100

    Initial Commit

---
+++ StatusBarNotification/StatusBarNotification.h
@@ -0,0 +1,19 @@
+//
+//  StatusBarNotification.h
+//  StatusBarNotification
+//
+//  Created by David Gölzhäuser on 11.02.17.
+//  Copyright © 2017 David Gölzhäuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StatusBarNotification.
+FOUNDATION_EXPORT double StatusBarNotificationVersionNumber;
+
+//! Project version string for StatusBarNotification.
+FOUNDATION_EXPORT const unsigned char StatusBarNotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StatusBarNotification/PublicHeader.h>
+
+
